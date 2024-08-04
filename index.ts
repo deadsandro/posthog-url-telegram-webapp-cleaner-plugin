9777a0a1bb42005c4c51ee277f4a8c7693c6da11
deadsandro <deadsandro@gmail.com>
2024-08-04 23:35:57 +0300

    add debug

--- index.ts
+++ index.ts
@@ -18,19 +18,25 @@ export function processEvent(
     event: PostHogEvent,
     meta: PluginMeta<PluginInput>
 ) {
-    const $current_url = event?.properties?.$current_url;
+    console.info('event', event);
 
-    if (event?.properties && $current_url) {
-        const normalized_url = cleanTelegramAuthUrl($current_url);
+    const current_url = event?.properties?.$current_url;
+    console.info('$current_url', current_url);
+    if (current_url) {
+        console.info(`$current_url 1: ${current_url}`);
+        const normalized_url = cleanTelegramAuthUrl(current_url);
+        console.info(`$current_url 2 : ${current_url}`);
+        console.info(`cleaned_url 3 : ${normalized_url}`);
         event.properties.$current_url = normalized_url;
-        console.debug(`cleaned_url: ${normalized_url}`);
+        console.info(`cleaned_url: ${normalized_url}`);
     }
 
     const $initial_current_url = event?.properties?.$initial_current_url;
-    if (event?.properties && $initial_current_url) {
+    console.info('$initial_current_url', $initial_current_url);
+    if ($initial_current_url) {
         const normalized_url = cleanTelegramAuthUrl($initial_current_url);
         event.properties.$initial_current_url = normalized_url;
-        console.debug(`cleaned_url: ${normalized_url}`);
+        console.info(`cleaned_url: ${normalized_url}`);
     }
 
     return event;
